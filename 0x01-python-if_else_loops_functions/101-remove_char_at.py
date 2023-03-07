42c405f93b4656576a2423adade13e5287aa1d6c
Samuel <a2jsamuel@gmail.com>
2023-03-07 18:27:41 +0000

    add 101 file

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    tempo = ""
     for i in range(len(str)):
-        if i == n:
-            return(str.replace(str[i], ""))
-    return(str)
+        if (i == n):
+            continue
+        tempo = tempo + str[i]
+    return tempo
