eaff6b952cfa2d83a8a6e5154d89875cb535ace7
Samuel <a2jsamuel@gmail.com>
2023-03-09 12:09:32 +0000

    add 0-add file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
+    from add_0 import add as z
     x = 1
     y = 2
-    print("{} + {} = {}".format(x, y, add(x, y)))
+    print("{} + {} = {}".format(x, y, z(x, y)))
