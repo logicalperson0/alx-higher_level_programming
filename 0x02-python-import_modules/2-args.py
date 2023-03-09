bc7c7bee7c06244b86eb386cb098d1de2bf52e6f
Samuel <a2jsamuel@gmail.com>
2023-03-09 13:54:01 +0000

    add 2nd- file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+
+elif len(sys.argv) > 2:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -4,4 +4,6 @@
 1. 1-calculation.py is a program that imports functions from the file calculator_1.py,
    does some Maths, and prints the result
 
-2.
+2. 2-args.py is a program that prints the number of and the list of its arguments.
+
+3.
