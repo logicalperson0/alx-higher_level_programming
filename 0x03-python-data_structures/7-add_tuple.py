30b80257f968e3d524c34924cca93bea1a8a400c
Samuel <a2jsamuel@gmail.com>
2023-03-11 20:53:40 +0000

    fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        c = (0, 0)
-    elif len(tuple_b) < 1:
-        if len(tuple_a) == 1:
-            c = (tuple_a[0])
-        c = (tuple_a[0], tuple_a[1])
-    elif len(tuple_a) < 1:
-        if len(tuple_b) == 1:
-            c = (tuple_b[0])
-        c = (tuple_b[0], tuple_b[1])
-    elif len(tuple_a) < 2:
-        if len(tuple_b) < 2:
-            c = (tuple_a[0] + tuple_b[0])
-        c = (tuple_a[0] + tuple_b[0], tuple_b[1])
-    elif len(tuple_b) < 2:
-        if len(tuple_a) < 2:
-            c = (tuple_a[0] + tuple_b[0])
-        c = (tuple_a[0] + tuple_b[0], tuple_a[1])
+    len_x = len(tuple_a)
+    len_y = len(tuple_b)
+
+    if len_x == 0:
+        i = 0
+        j = 0
+    elif len_x == 1:
+        i = tuple_a[0]
+        j = 0
     else:
-        c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return c
+        i = tuple_a[0]
+        j = tuple_a[1]
+
+    if len_y == 0:
+        c = 0
+        d = 0
+    elif len_y == 1:
+        c = tuple_b[0]
+        d = 0
+    else:
+        c = tuple_b[0]
+        d = tuple_b[1]
+
+    v = (i + c, j + d)
+    return v
