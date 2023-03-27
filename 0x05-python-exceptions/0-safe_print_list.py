89a8b70ed96f646ba8829fa4e02ac121f4543651
Samuel <a2jsamuel@gmail.com>
2023-03-27 11:08:03 +0000

    add 0-safe_print_list.py for task 0 in Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+def safe_print_list(my_list=[], x=0):
+    z = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except:
+            break
+        else:
+            z = z + 1
+    print()
+
+    return (z)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,3 @@
+0-safe_print_list.py is a function that prints x elements of a list.
+
+
