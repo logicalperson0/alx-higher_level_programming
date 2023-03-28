a44892bca1de3edb4cd26d330e1e59deb21ce3aa
Samuel <a2jsamuel@gmail.com>
2023-03-28 10:18:05 +0000

    add 0-square.py class for 0 task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """A empty class called Square:
+    that defines square
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,5 @@
+This is a readme for the python class lesson:
+
+0-square.py is a empty class Square that defines a square
+
+
