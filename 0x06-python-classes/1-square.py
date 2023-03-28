93a2c53eac2c643c78837ccd8dc8d99eac82f502
Samuel <a2jsamuel@gmail.com>
2023-03-28 10:35:48 +0000

    add 1-square.py class file for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defining a Square"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int): size of square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+        Arg:
+            size: size of square
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -2,4 +2,6 @@ This is a readme for the python class lesson:
 
 0-square.py is a empty class Square that defines a square
 
+1-square.py is a class Square that defines a square by: (based on 0-square.py)
+
 
