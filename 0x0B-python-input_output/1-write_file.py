96debf7117a513c70a672ca7f781458d95ff4076
Samuel <a2jsamuel@gmail.com>
2023-04-11 11:21:45 +0000

    1-write_file.py file for task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function called write_file"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a
+    text file (UTF8) and returns the number of
+    char written
+    Arg:
+        filename: file to write to
+        text: txt to write
+    Return:
+        None
+    """
+    chars = 0
+
+    with open(filename, 'w', encoding='UTF8') as w:
+        w.write(text)
+
+    with open(filename, 'r') as r:
+        for x in r.read():
+            chars += 1
+
+    return (chars)
