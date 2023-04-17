2f8242c9f3c8091e8f228ab70cdaa84309789293
Samuel <a2jsamuel@gmail.com>
2023-04-17 17:52:45 +0000

    fix for square for task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -49,20 +49,20 @@ class Square(Rectangle):
                 if x == 0:
                     self.id = arg
                 if x == 1:
-                    self.width = arg
+                    self.size = arg
                 if x == 2:
                     self.x = arg
                 if x == 3:
                     self.y = arg
         else:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'size' in kwargs:
-                self.width = kwargs['size']
-            if 'x' in kwargs:
-                self.x = kwargs['x']
-            if 'y' in kwargs:
-                self.y = kwargs['y']
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.width = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
     def to_dictionary(self):
         """returns a dict of class square
