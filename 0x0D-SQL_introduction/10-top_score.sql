d36e00d5ce870e00db7fb315ceae9f91244f30cb
Samuel <a2jsamuel@gmail.com>
2023-05-16 15:53:07 +0000

    add 10-top_score.sql for list by score in task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List by best
+-- script that lists all records of the table second_table of the database hbtn_0c_0
+-- display both the score and the name (in this order)
+SELECT score, name
+  FROM second_table
+  ORDER BY score DESC, name DESC
