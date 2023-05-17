e2ca7456f09f6c76b3f69b7f767fc68d5bb5fb98
Samuel <a2jsamuel@gmail.com>
2023-05-17 15:06:13 +0000

    add 9-cities_by_state_join.sql file for task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.id = states.id;
+JOIN states ON cities.state_id = states.id;
