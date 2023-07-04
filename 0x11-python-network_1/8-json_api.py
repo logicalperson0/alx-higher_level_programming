0fd7e8803108b56089964d21e05f64a915e5531e
Samuel <a2jsamuel@gmail.com>
2023-07-04 21:58:51 +0300

    Fix and add 8-file!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,16 +6,21 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
+
 if __name__ == "__main__":
     urls = "http://0.0.0.0:5000/search_user"
     var = {'q': ""}
+
     if len(sys.argv) > 1:
-        val['q'] = sys.argv[1]
+        var['q'] = sys.argv[1]
+    else:
+        ""
+
+    resp = requests.post(urls, data=var)
 
     try:
-        resp = requests.post(urls, data=var)
         res = resp.json()
-        if not res:
+        if res == {}:
             print("No result")
         else:
             print("[{}] {}".format(res.get('id'), res.get('name')))
