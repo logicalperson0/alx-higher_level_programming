90e856a3eca5c77edf04aa121267f72429f74db0
Samuel <a2jsamuel@gmail.com>
2023-07-25 22:07:50 +0300

    This is for adv task 7!

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
 const requesting = require('request');
 const urls = process.argv[2];
-let dict = {};
+const dict = {};
 
 requesting(urls, (error, response, body) => {
-  if (error) console.log(error);
+  if (error) {
+    console.log(error);
+  } else {
+    const parsing = JSON.parse(body);
 
-  const parsing = JSON.parse(body);
-
-  for (let x of parsing) {
-    if (x.completed === true) {
-      if (dict[x.userId] === undefined) {
-        dict[x.userId] = 1;
-      } else {
-        dict[x.userId]++;
+    for (const x of parsing) {
+      if (x.completed === true) {
+        if (dict[x.userId] === undefined) {
+          dict[x.userId] = 1;
+        } else {
+          dict[x.userId]++;
+        }
       }
     }
+    console.log(dict);
   }
-  console.log(dict);
 });
