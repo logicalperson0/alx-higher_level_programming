247849bc7dee7e4ef0cbf0a0ec977e0141cab768
Samuel <a2jsamuel@gmail.com>
2023-07-25 15:01:36 +0300

    This is for task 0 of web scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const file = require('fs');
+file.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (data === undefined) {
+    console.log(err);
+    } else {
+      console.log(data);
+    }
+});
