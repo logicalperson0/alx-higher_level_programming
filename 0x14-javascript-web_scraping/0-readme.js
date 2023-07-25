bfa6e85c82781c96606096efbef18c0987b5e4b0
Samuel <a2jsamuel@gmail.com>
2023-07-25 15:03:40 +0300

    Semistandard for 0-file.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const file = require('fs');
 file.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (data === undefined) {
     console.log(err);
-    } else {
-      console.log(data);
-    }
+  } else {
+    console.log(data);
+  }
 });
