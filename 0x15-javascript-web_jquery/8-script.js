5809e47ee463d25b5763b1fb3be5463808230a4c
Dark Mind <dune934@gmail.com>
2023-08-03 00:14:21 +0300

    Fix and add 8.js file

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,15 @@
-const $ = window.$;
-$.get('https://swapi.co/api/films/?format=json', function (data, textStatus) {
-  const res = data.results;
-  for (let i = 0; i < res.length; i++) {
-    $('UL#list_movies').append('<li>' + res[i].title + '</li>');
+const moviesUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const $movieListing = $('#list_movies');
+
+$.ajax({
+  url: moviesUrl,
+  dataType: 'json'
+}).done((data) => {
+  const Starmovies = data.results;
+
+  for (let x = 0; x < Starmovies.length; x++) {
+    const starTitle = Starmovies[x].title;
+    const li = `<li>${starTitle}</li>`;
+    $movieListing.append(li);
   }
 });
